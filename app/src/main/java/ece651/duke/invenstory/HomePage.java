2877a04ad138b6c84628c95cc700fcf0c68d221f
jab165@duke.edu <jab165@duke.edu>
2016-02-22 01:21:31 -0500

    Still working on camera work line 850 unlockfocus() < ~200 lines left

--- app/src/main/java/ece651/duke/invenstory/HomePage.java
+++ app/src/main/java/ece651/duke/invenstory/HomePage.java
@@ -1,6 +1,7 @@
 package ece651.duke.invenstory;
 
 import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -28,7 +29,7 @@ public class HomePage extends AppCompatActivity implements View.OnClickListener{
         switch(v.getId()){
             case R.id.btnCamera:
                 //Do stuff for if camera button is clicked
-                startActivity(new Intent(this, Camera.class));
+                startActivity(new Intent(this, PhotoActivity.class));
                 break;
             case R.id.btnList:
                 startActivity(new Intent(this, ShowList.class));
